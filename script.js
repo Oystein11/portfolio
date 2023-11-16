e92e34db181baeb569799a64297c28779cea3abb
Øystein <oystein.koder@gmail.com>
2023-11-16 14:56:10 +0100

    edit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-//mode toggle
+//Light/Dark mode toggle
 function theme_toggle () {
     const body = document.querySelector ("body");
     body.classList.toggle ("dark_mode");
     console.log ("Mode change");
 }
+
+//Hamburger-meny
